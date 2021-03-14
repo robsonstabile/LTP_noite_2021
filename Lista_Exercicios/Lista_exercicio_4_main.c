77c4aa37ac531f37d947978fb9f5bf1e99bd5e0e
robsonstabile <80135246+robsonstabile@users.noreply.github.com>
2021-03-14 15:29:40 -0300

    Lista_exercício_4

---
+++ Lista_Exercicios/Lista_exercicio_4_main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main(void) {
+
+  int e_num_1, e_num_2, e_num_3;
+
+  printf("Entre com o 1° número:\n");
+  scanf("%i", &e_num_1);
+
+  printf("Entre com o 2° número:\n");
+  scanf("%i", &e_num_2);
+
+  printf("Entre com o 3° número:\n");
+  scanf("%i", &e_num_3);
+
+  if (e_num_1<e_num_2){
+
+   if(e_num_2<e_num_3){
+     printf("Ordem decrescente:\n%i\n%i\n%i\n",e_num_3,e_num_2,e_num_1);
+   }else if (e_num_1<e_num_3){
+     printf("Ordem decrescente:\n%i\n%i\n%i\n",e_num_2,e_num_3,e_num_1);
+   }else {
+     printf("Ordem decrescente:\n%i\n%i\n%i\n",e_num_2, e_num_1, e_num_3);
+   }
+   }else if (e_num_1<e_num_3){
+     printf("Ordem decrescente:\n%i\n%i\n%i\n",e_num_3,e_num_1,e_num_2);
+   }else if(e_num_2<e_num_3){
+     printf("Ordem decrescente:\n%i\n%i\n%i\n",e_num_1,e_num_3,e_num_2);
+   }else {
+     printf("Ordem decrescente:\n%i\n%i\n%i\n",e_num_1,e_num_2,e_num_3);
+   }
+
+  return 0;
+}
