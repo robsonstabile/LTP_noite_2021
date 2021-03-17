3d4ff34d131add5edbc1f5a1522ae2418ce34f7a
robsonstabile <80135246+robsonstabile@users.noreply.github.com>
2021-03-17 11:05:48 -0300

    Lista_exercício_7

---
+++ Lista_Exercicios/Lista_exercício_7_main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(void) {
+
+  float e_r1, e_r2, e_r3, e_r4, s_req;
+
+  printf("Calculadora para Resistor em Série\n\n");
+
+  printf("Entre com o Valor do 1° Resistor em [ohms]:\n ");
+  scanf("%f",&e_r1);
+
+  printf("Entre com o Valor do 2° Resistor em [ohms]:\n ");
+  scanf("%f",&e_r2);
+
+  printf("Entre com o Valor do 3° Resistor em [ohms]:\n ");
+  scanf("%f",&e_r3);
+
+  printf("Entre com o Valor do 4° Resistor em [ohms]:\n ");
+  scanf("%f",&e_r4);
+
+  s_req = e_r1 + e_r2 + e_r3 + e_r4;
+  
+  printf("Resistência Equivalente em [ohms]: \n %.2f\n",s_req);
+
+  return 0;
+}
