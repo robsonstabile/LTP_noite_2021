b299c4b24703323bb0f623cc3a9a44d6b694b8ad
robsonstabile <80135246+robsonstabile@users.noreply.github.com>
2021-03-17 10:31:14 -0300

    Lista_exercício_7_código em C

---
+++ Lista_Exercicios/Lista_exercício_7_main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(void) {
+
+  float e_r1, e_r2, e_r3, e_r4, s_req;
+
+  printf("Entre com o Valor do 1° Resistor em [homs]:\n ");
+  scanf("%f",&e_r1);
+
+  printf("Entre com o Valor do 2° Resistor em [homs]:\n ");
+  scanf("%f",&e_r2);
+
+  printf("Entre com o Valor do 3° Resistor em [homs]:\n ");
+  scanf("%f",&e_r3);
+
+  printf("Entre com o Valor do 4° Resistor em [homs]:\n ");
+  scanf("%f",&e_r4);
+
+  s_req = e_r1 + e_r2 + e_r3 + e_r4;
+  printf("Resistência Equivalente em [homs]: \n %.f\n",s_req);
+
+  return 0;
+}
